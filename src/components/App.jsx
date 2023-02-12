17f6bef89a878794f7d3e89e5d8f083a9c0c7e77
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2023-02-12 19:40:06 +0200

    remove store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,19 @@ import routes from 'utils/constants';
 // import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
-// import { lazy } from 'react';
+import { lazy } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import Loader from './Loader/Loader';
 // import PrivateRoute from './Routes/PrivateRoute';
 // import PublicRoute from './Routes/PublicRoute';
 
-// const Home = lazy(() => import('pages/Home/Home.js'));
+const Home = lazy(() => import('pages/Home/Home.js'));
 
 const App = () => {
   return (
     <Routes>
       <Route path={routes.home} element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="*" element={<Navigate to={routes.home} replace />} />
       </Route>
     </Routes>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import { store, persistor } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/react-project">
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}> */}
+    <App />
+    {/* </PersistGate>
+    </Provider> */}
   </BrowserRouter>
 );
