c42e0aa4dfcac840a91c677f4177a1e1407b9565
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2023-02-12 19:46:53 +0200

    remove browserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import { store, persistor } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
