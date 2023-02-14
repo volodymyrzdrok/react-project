fbc7a26f350b5e4d8cea16de2f34334c697708a4
Andrii-Posternak <panposternak@gmail.com>
2023-02-14 08:03:37 +0200

    adds variables, fonts, routes in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-import routes from 'utils/constants';
 // import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Layout from './Layout/Layout';
 import { lazy } from 'react';
+import routes from 'utils/constants';
+import Layout from 'components/Layout/Layout';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage.js'));
+const RegistrationPage = lazy(() =>
+  import('pages/RegistrationPage/RegistrationPage.js')
+);
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage.js'));
+const StatisticsPage = lazy(() =>
+  import('pages/StatisticsPage/StatisticsPage.js')
+);
+const DashboardPage = lazy(() =>
+  import('pages/DashboardPage/DashboardPage.js')
+);
 // import { useDispatch, useSelector } from 'react-redux';
 // import Loader from './Loader/Loader';
 // import PrivateRoute from './Routes/PrivateRoute';
 // import PublicRoute from './Routes/PublicRoute';
 
-const Home = lazy(() => import('pages/HomePage/HomePage.js'));
-
 export const App = () => {
   return (
     <Routes>
       <Route path={routes.home} element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<HomePage />} />
+        <Route path={routes.register} element={<RegistrationPage />} />
+        <Route path={routes.login} element={<LoginPage />} />
+        <Route path={routes.statistics} element={<StatisticsPage />} />
+        <Route path={routes.dashboard} element={<DashboardPage />} />
         <Route path="*" element={<Navigate to={routes.home} replace />} />
       </Route>
     </Routes>
