8a0171a09d3d0a2271598079e5a63ee359b2d4d1
volodymyrzdrok <58653496+volodymyrzdrok@users.noreply.github.com>
2023-02-16 11:00:02 +0000

    Deploying to gh-pages from @ volodymyrzdrok/react-project@ecdecc35ea0452cab86ae214e2698aa691f2a54b 🚀

--- static/js/162.eb1d03e4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_project=self.webpackChunkreact_project||[]).push([[162],{4162:function(e,c,r){r.r(c);r(2791);var t=r(184);c.default=function(){return(0,t.jsx)("div",{children:" \u0442\u0430\u0431\u043b\u0438\u0446\u044f"})}}}]);
-//# sourceMappingURL=162.eb1d03e4.chunk.js.map
