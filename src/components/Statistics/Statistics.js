fd8e53dd2a24835dbc7f781e4ab962549ac2ca9d
Andrii-Posternak <panposternak@gmail.com>
2023-02-17 16:57:45 +0200

    adds diagram, colors

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,37 +1,81 @@
 import React from 'react';
-
-import s from './Statistics.module.scss';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { getTransactionsStats } from 'redux/transactionsSummary/transactionsSummaryOperations';
 import Table from '../Table/Table';
+import Chart from '../Chart/Chart';
+import colors from './colors';
+import s from './Statistics.module.scss';
+
+const formatData = transactions => {
+  const dataTotal = [];
+  const dataName = [];
+
+  if (transactions.length === 0) {
+    return { dataTotal: [100], dataName };
+  }
+
+  for (const category of transactions) {
+    dataTotal.push(category.total);
+    dataName.push(category.name);
+  }
+
+  return { dataTotal, dataName };
+};
+
+const filterColors = categories => {
+  const filteredColorsArr = [];
+  const colorArr = Object.entries(colors);
+  for (const category of categories) {
+    const filteredColor = colorArr
+      .filter(el => el[0] === category)
+      .flatMap(el => el[1]);
+    filteredColorsArr.push(filteredColor);
+  }
+  return filteredColorsArr;
+};
 
 const Statistics = () => {
   const dispatch = useDispatch();
+
+  const { transactionsSummary } = useSelector(state => state.summary);
 
   const onClickM = () => {
     dispatch(getTransactionsStats({ month: 2, year: 2023 }));
   };
 
   const onClickY = () => {
-    dispatch(getTransactionsStats({ month: 3, year: 2023 }));
+    dispatch(getTransactionsStats({ month: 4, year: 2023 }));
   };
 
-  const { transactionsSummary } = useSelector(state => state.summary);
+  const transactionData = transactionsSummary?.categoriesSummary || [];
+  const { dataTotal, dataName } = formatData(transactionData);
+  const filteredColors = filterColors(dataName);
+  const data = {
+    labels: dataName,
+    datasets: [
+      {
+        label: 'Total',
+        data: dataTotal,
+        backgroundColor: filteredColors.length ? filteredColors : 'gray',
+        borderColor: 'transparent',
+        cutout: '70%',
+      },
+    ],
+  };
 
   return (
     <>
       <div className={s.contentWrapper}>
         <div className={s.diagramBlock}>
-          Diagram
-          {/* <Chart /> */}
+          Statistic
+          <Chart data={data} periodTotal={transactionsSummary?.periodTotal} />
         </div>
         <div className={s.transactionsSummaryBlock}>
           <div className={s.selectorsBlock}>
             <button onClick={onClickM}>February</button>
             <button onClick={onClickY}>March</button>
           </div>
-          <Table {...transactionsSummary} />
+          <Table {...transactionsSummary} colors={filteredColors} />
         </div>
       </div>
     </>
