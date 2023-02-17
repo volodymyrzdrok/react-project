ce921b5fe18b91348f5b7fe5306d951e07e5f9e9
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2023-02-17 13:29:01 +0200

    basic html

--- src/components/ModalLogout/ModalLogout.js
+++ src/components/ModalLogout/ModalLogout.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import s from './ModalLogout.module.scss';
+import logoutImg from '../../assets/images/logoutImg.png';
 
 const ModalLogout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <img src={logoutImg} alt="Businessman Thinking" />
+      <p>Are you sure, you want to Log Out?</p>
+      <div className={s.btnContainer}>
+        <button>Keep in</button>
+        <button>Log out</button>
+      </div>
+    </div>
+  );
 };
 
 export default ModalLogout;
