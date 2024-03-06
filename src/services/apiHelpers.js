175f51a725f1f29bf53d74d3cc3d6d4dfda752f5
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-03-06 16:49:36 +0200

    changed base url for project

--- src/services/apiHelpers.js
+++ src/services/apiHelpers.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://wallet.goit.ua';
+axios.defaults.baseURL = 'https://wallet.b.goit.study';
 
 export const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
